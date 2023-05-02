34ddbb3405cff8cdd3b549826bb590f7eb10acd8
Luis Rivera <ldr5468@gmail.com>
2023-05-02 18:41:39 -0400

    Commit 1

--- imageTest.py
+++ imageTest.py
@@ -1,6 +1,4 @@
 import os
-import time
-import keyboard
 import numpy as np
 import cv2
 import tensorflow as tf
@@ -10,45 +8,39 @@ from sense_hat import SenseHat
 print(tf.version.VERSION)
 sense = SenseHat()
 
-
 loaded_model = tf.keras.models.load_model('/home/pi/handrecog/keras_convnet_adam')
 
 loaded_model.summary()
 
 cap = cv2.VideoCapture(0)
 
-#sense.show_message("Start")
-
-# define the contrast and brightness value
-contrast = 0. # Contrast control ( 0 to 127)
-brightness = 0. # Brightness control (0-100)
+count = 0
 
 while(True):
     ret, raw_bgr = cap.read()
-    #cv2.normalize(frame, frame, 20, 235, cv2.NORM_MINMAX)
     gray = cv2.cvtColor(raw_bgr, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
-    #cv2.imwrite("img.jpg", frame)
-    #raw_bgr = cv2.imread("img.jpg")
     bgr_inverted = cv2.threshold(raw_bgr, 50, 255, cv2.THRESH_BINARY_INV)
-    #raw_contrast_adj = cv2.addWeighted( raw_bgr, contrast, raw_bgr, 0, brightness)
     resized_bgr = cv2.resize(bgr_inverted[1], (28, 28), interpolation = cv2.INTER_CUBIC)
-    #cv2.imshow('processed image', img)
-    #print(img.shape)
     rgb = cv2.cvtColor(resized_bgr, cv2.COLOR_BGR2RGB)
-    rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.float32)
+    rgb_tensor = (tf.convert_to_tensor(rgb, dtype=tf.float32) / 255.0)
     gray_tensor = tf.image.rgb_to_grayscale(rgb_tensor)
-    #print(img.shape)
     cv2.imwrite("preview.jpg", gray_tensor.numpy())
     gray_tensor = tf.expand_dims(gray_tensor, axis=0)
-    #print(img.shape)
     y_hat = loaded_model.predict(gray_tensor)
-    print(f"{y_hat}")
+
     prediction= np.argmax(y_hat)
     score = y_hat[[0],[prediction]].item()
-    print(score)
-    print('Number is a {} with a certainty of {:.2%}'.format(prediction, score))
-    sense.show_letter(str(prediction))
+    if score >= 0.7:
+        print('Number is a {} with a certainty of {:.2%}'.format(prediction, score))
+        sense.show_letter(str(prediction))
+    else:
+        if count % 2:
+            sense.show_letter('/')
+        else:
+            sense.show_letter('\\')
+        count = count + 1
+
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- keras_convnet.py
+++ keras_convnet.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Model / data parameters
 num_classes = 10
@@ -29,36 +29,29 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 model = keras.models.Sequential()
 model.add(keras.layers.Conv2D(32, (2, 2), input_shape = (28, 28, 1), activation = 'relu'))
 model.add(keras.layers.MaxPooling2D(pool_size = (2,2)))
-model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.Conv2D(32, (2, 2), input_shape = (28, 28, 1), activation = 'relu'))
+model.add(keras.layers.MaxPooling2D(pool_size = (2,2)))
+model.add(keras.layers.Dropout(0.2))
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(units = 50, activation = 'relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(units = 100, activation = 'relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(units = 500, activation = 'relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(units = 1000, activation = 'relu'))
 model.add(keras.layers.Dense(units = 10, activation = 'softmax'))
 
-model.summary()
-
 batch_size = 128
-epochs = 10
+epochs = 5
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+print(model.summary())
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2, verbose=1)
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
 
-#plt.plot(history.history['loss'])
-#plt.plot(history.history['accuracy'])
-#plt.title('model loss')
-#plt.ylabel('loss & accuracy')
-#plt.xlabel('epoch')
-#plt.show()
+plt.plot(history.history['loss'])
+plt.plot(history.history['accuracy'])
+plt.title('model loss')
+plt.ylabel('loss & accuracy')
+plt.xlabel('epoch')
+plt.show()
 
-#model.save('C:/Users/ldr54/Desktop/keras_convnet_adam')
 model.save('/home/pi/handrecog/keras_convnet_adam')
